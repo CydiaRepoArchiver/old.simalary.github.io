92c8e9f6058a4b4c8f13bc05c17b0e8b1d19fc7f
simalary <harper.chris033@live.com>
2016-08-02 19:45:48 +0000

    added Repo Icons

--- packages.sh
+++ packages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/Foreground debs
 dpkg-deb -bZgzip projects/CydiaMultitaskingIconFix debs
+dpkg-deb -bZgzip projects/RepoIcons debs
 #dpkg -bZgzip projects/<projectname> debs
