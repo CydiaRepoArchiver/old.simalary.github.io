e628652f1272d7719ce5cd074b0e9df74bfab579
simalary <harper.chris033@live.com>
2016-08-03 01:14:52 +0000

    Added El Capitan Beach Ball Loading Indicator and changed some repo icons

--- packages.sh
+++ packages.sh
@@ -2,4 +2,5 @@
 dpkg-deb -bZgzip projects/Foreground debs
 dpkg-deb -bZgzip projects/CydiaMultitaskingIconFix debs
 dpkg-deb -bZgzip projects/RepoIcons debs
+dpkg-deb -bZgzip projects/ElCapitanBeachBallLoadingIndicator debs
 #dpkg -bZgzip projects/<projectname> debs
