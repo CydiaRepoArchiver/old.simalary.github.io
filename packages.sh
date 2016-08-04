26a8a80c78f0713cc9187c5a305b05590c722af7
simalary <harper.chris033@live.com>
2016-08-04 21:10:41 +0000

    Added Cream

--- packages.sh
+++ packages.sh
@@ -3,4 +3,5 @@ dpkg-deb -bZgzip projects/SpectrumBETA debs
 dpkg-deb -bZgzip projects/CydiaMultitaskingIconFix debs
 dpkg-deb -bZgzip projects/RepoIcons debs
 dpkg-deb -bZgzip projects/ElCapitanBeachBallLoadingIndicator debs
+dpkg-deb -bZgzip projects/Cream debs
 #dpkg -bZgzip projects/<projectname> debs
